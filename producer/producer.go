29677cea12b2534fc8eb599110d4b0c93233c527
David Vavra <david.vavra@firma.seznam.cz>
2020-01-16 13:50:17 +0100

    squash: cr

--- producer/producer.go
+++ producer/producer.go
@@ -2,21 +2,21 @@ package producer
 
 import (
 	"net"
+	"net/url"
 	"time"
 )
 
 type RequestEvent struct {
-	Time           time.Time
-	IP             net.IP
-	StatusCode     int
-	Duration       time.Duration
-	Path           string
-	NormalizedPath string
-	UserAgent      string
-	Method         string
-	Params         map[string]string
-	SloClass       string
-	SloApp         string
-	SloEndpoint    string
-	SloDomain      string
+	Time          time.Time
+	IP            net.IP
+	StatusCode    int
+	Duration      time.Duration
+	URL           *url.URL
+	NormalizedURL *url.URL
+	Headers       map[string]string
+	Method        string
+	SloClass      string
+	SloApp        string
+	SloEndpoint   string
+	SloDomain     string
 }
