d32390a9056b1c8dae5747939eec8fe2860e0e20
Lukáš Svoboda <lukas.svoboda2@firma.seznam.cz>
2020-01-22 22:43:11 +0100

    feat: added http_tail.sh for tailing remote logs

---
+++ scripts/http_tail.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+URL=${URL:-"https://skweb1.ko.seznam.cz:8888/szn-sklik-userproxy/access_log"}
+
+if [ -z "$USER" -o -z "$PASSWD" ]; then
+  echo "Envs USER and PASSWD are mandatory!"
+  exit 1
+fi
+
+while(true); do
+    sleep 2;
+    wget -c  --password $PASSWD --user lukas.svoboda2  -o /dev/null --no-check-certificate  "$URL"
+done
