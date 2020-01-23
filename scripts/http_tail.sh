1e0c899516b2a1514da06a81b78b50685ba48df7
Martin Chodur <m.chodur@seznam.cz>
2020-01-23 12:47:56 +0100

    chore: fixed compose tailing and speedup setup
    Signed-off-by: Martin Chodur <m.chodur@seznam.cz>

--- scripts/http_tail.sh
+++ scripts/http_tail.sh
@@ -2,12 +2,12 @@
 
 URL=${URL:-"https://skweb1.ko.seznam.cz:8888/szn-sklik-userproxy/access_log"}
 
-if [ -z "$USER" -o -z "$PASSWD" ]; then
-  echo "Envs USER and PASSWD are mandatory!"
+if [ -z "$SZN_LOGY_USER" -o -z "$SZN_LOGY_PASSWORD" ]; then
+  echo "Envs SZN_LOGY_USER and SZN_LOGY_PASSWORD are mandatory!"
   exit 1
 fi
 
 while(true); do
+    wget -c  --password "$SZN_LOGY_PASSWORD" --user "$SZN_LOGY_USER" --tries=3 -o /dev/null --no-check-certificate  "$URL";
     sleep 2;
-    wget -c  --password "$PASSWD" --user "$USER"  -o /dev/null --no-check-certificate  "$URL"
 done
