9f8ca7b279ef6d702d964d1384d554346b29a5e4
David Vavra <david.vavra@firma.seznam.cz>
2020-02-20 14:33:53 +0100

    refactor: cr

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -13,38 +13,38 @@ function get_metrics {
 
 function evaluate_test_result {
     while read line ; do
-        if ! grep -q "$line" ${TEST_RESULT_DIR}/${METRICS_FN}; then
+        if ! grep -q "$line" ${TEST_RESULT_DIR}/${METRICS_FILENAME}; then
             echo "  FAIL: Missing the following metric!"
             echo "$line"
             exit 1
         fi
-    done < ${METRICS_FN}
+    done < ${METRICS_FILENAME}
     echo "  OK: found all expected metrics"
 }
 
-SCRIPT_FN=$(readlink -f $0)
-SLO_EXPORTER="$( dirname "${SCRIPT_FN}" )/../slo_exporter"
+SCRIPT_DIR=$( dirname "$(readlink -f $0)" )
+SLO_EXPORTER="${SCRIPT_DIR}/../slo_exporter"
 
 TEST_DIR_PREFIX="Test_"
 TEST_RESULT_DIR="test_ouput"
 
-CONFIG_FN="slo_exporter.yaml"
+CONFIG_FILENAME="slo_exporter.yaml"
 METRICS_URL="http://localhost:8080/metrics"
-METRICS_FN="metrics"
-EXPECTED_METRICS_FN="metrics.expected"
+METRICS_FILENAME="metrics"
+EXPECTED_METRICS_FILENAME="metrics.expected"
 
-SLO_EXPORTER_LOG_FN="slo_exporter.log"
+SLO_EXPORTER_LOG_FILENAME="slo_exporter.log"
 
 cleanup
 
-for i_test in $(find $(dirname "${SCRIPT_FN}" ) -type d | grep ${TEST_DIR_PREFIX}) ; do
+for i_test in $(find "${SCRIPT_DIR}" -type d | grep ${TEST_DIR_PREFIX}) ; do
     echo "${i_test}"
 
     pushd ${i_test} > /dev/null
     mkdir ${TEST_RESULT_DIR}
-    ${SLO_EXPORTER} --config-file=${CONFIG_FN} --disable-timescale-exporter > ${TEST_RESULT_DIR}/${SLO_EXPORTER_LOG_FN} 2>&1 &
+    ${SLO_EXPORTER} --config-file=${CONFIG_FILENAME} --disable-timescale-exporter > ${TEST_RESULT_DIR}/${SLO_EXPORTER_LOG_FILENAME} 2>&1 &
     sleep 1
-    get_metrics > ${TEST_RESULT_DIR}/${METRICS_FN}
+    get_metrics > ${TEST_RESULT_DIR}/${METRICS_FILENAME}
     # kill slo exporter test instance
     kill %1 || true
 
