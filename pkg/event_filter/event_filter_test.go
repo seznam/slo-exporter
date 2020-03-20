32f8674cca3a3206f911e2f89bfc024691a5c3da
Chodur, Martin <martin.chodur@firma.seznam.cz>
2020-03-20 12:37:41 +0100

    feat event_filter: added filter using metadata and dropped the old ones
    Signed-off-by: Martin Chodur <martin.chodur@firma.seznam.cz>

--- pkg/event_filter/event_filter_test.go
+++ pkg/event_filter/event_filter_test.go
@@ -8,63 +8,42 @@ import (
 )
 
 type ShouldDropTestCase struct {
-	eventFilter *RequestEventFilter
+	eventFilter *EventFilter
 	event       *event.HttpRequest
 	dropped     bool
 	reason      string
 }
 
-func TestEventFilter_statusMatch(t *testing.T) {
-	config := eventFilterConfig{
-		FilteredHttpStatusCodeMatchers: []string{"301", "40[04]"},
-	}
-	testCases := []struct {
-		statusCode  int
-		shouldMatch bool
-	}{
-		{statusCode: 200, shouldMatch: false},
-		{statusCode: 301, shouldMatch: true},
-		{statusCode: 400, shouldMatch: true},
-		{statusCode: 404, shouldMatch: true},
-		{statusCode: 418, shouldMatch: false},
-		{statusCode: 500, shouldMatch: false},
-	}
-	eventFilter, err := NewFromConfig(config)
-	if err != nil {
-		t.Error(err)
-	}
-	for _, tc := range testCases {
-		assert.Equal(t, tc.shouldMatch, eventFilter.statusMatch(tc.statusCode))
-	}
-}
-
 func TestEventFilter_headersMatch(t *testing.T) {
 	config := eventFilterConfig{
-		FilteredHttpHeaderMatchers: stringmap.StringMap{"(?i)User-Agent": "(?i)Firefox"},
+		MetadataFilter: stringmap.StringMap{"(?i)User-Agent": "(?i)Firefox"},
 	}
 	testCases := []struct {
-		headers     stringmap.StringMap
+		metadata    stringmap.StringMap
 		shouldMatch bool
 	}{
-		{headers: stringmap.StringMap{"foo": "bar"}, shouldMatch: false},
-		{headers: stringmap.StringMap{"useragent": "firefox"}, shouldMatch: false},
-		{headers: stringmap.StringMap{"user-agent": "firefox"}, shouldMatch: true},
-		{headers: stringmap.StringMap{"User-Agent": "Firefox"}, shouldMatch: true},
-		{headers: stringmap.StringMap{"User-Agent foo": "Firefox bar"}, shouldMatch: true},
+		{metadata: stringmap.StringMap{"foo": "bar"}, shouldMatch: false},
+		{metadata: stringmap.StringMap{"useragent": "firefox"}, shouldMatch: false},
+		{metadata: stringmap.StringMap{"user-agent": "firefox"}, shouldMatch: true},
+		{metadata: stringmap.StringMap{"User-Agent": "Firefox"}, shouldMatch: true},
+		{metadata: stringmap.StringMap{"User-Agent foo": "Firefox bar"}, shouldMatch: true},
 	}
 	eventFilter, err := NewFromConfig(config)
 	if err != nil {
 		t.Error(err)
 	}
 	for _, tc := range testCases {
-		assert.Equal(t, tc.shouldMatch, eventFilter.headersMatch(tc.headers))
+		matches, _ := eventFilter.metadataMatch(tc.metadata)
+		assert.Equal(t, tc.shouldMatch, matches)
 	}
 }
 
 func TestEventFilter_shouldDrop(t *testing.T) {
 	config := eventFilterConfig{
-		FilteredHttpStatusCodeMatchers: []string{"301", "404"},
-		FilteredHttpHeaderMatchers:     stringmap.StringMap{"(?i)name": "(?i)value"},
+		MetadataFilter: stringmap.StringMap{
+			"(?i)name":       "(?i)value",
+			"(?i)statusCode": "301|404",
+		},
 	}
 	eventFilter, err := NewFromConfig(config)
 	if err != nil {
@@ -74,68 +53,49 @@ func TestEventFilter_shouldDrop(t *testing.T) {
 		// no match
 		{
 			eventFilter,
-			&event.HttpRequest{
-				StatusCode: 200,
-			},
+			&event.HttpRequest{Metadata: stringmap.StringMap{"statusCode": "200"}},
 			false,
 			"",
 		},
 		// status code match
 		{
 			eventFilter,
-			&event.HttpRequest{
-				StatusCode: 301,
-			},
+			&event.HttpRequest{Metadata: stringmap.StringMap{"statusCode": "301"}},
 			true,
 			"status:301",
 		},
 		// no match
 		{
 			eventFilter,
-			&event.HttpRequest{
-				StatusCode: 200,
-				Headers:    stringmap.StringMap{"name1": "somevalue"},
-			},
+			&event.HttpRequest{Metadata: stringmap.StringMap{"name1": "somevalue"}},
 			true,
 			"",
 		},
 		// just header name match
 		{
 			eventFilter,
-			&event.HttpRequest{
-				StatusCode: 200,
-				Headers:    stringmap.StringMap{"name": "somevalue"},
-			},
+			&event.HttpRequest{Metadata: stringmap.StringMap{"statusCode": "200", "name": "somevalue"}},
 			true,
 			"",
 		},
 		// header match
 		{
 			eventFilter,
-			&event.HttpRequest{
-				StatusCode: 200,
-				Headers:    stringmap.StringMap{"name": "value"},
-			},
+			&event.HttpRequest{Metadata: stringmap.StringMap{"statusCode": "200", "name": "value"}},
 			true,
 			"header:name",
 		},
 		// header match, name normalization (->lower case)
 		{
 			eventFilter,
-			&event.HttpRequest{
-				StatusCode: 200,
-				Headers:    stringmap.StringMap{"Name": "Value"},
-			},
+			&event.HttpRequest{Metadata: stringmap.StringMap{"statusCode": "200", "Name": "Value"}},
 			true,
 			"header:name",
 		},
 		// both status code and header match
 		{
 			eventFilter,
-			&event.HttpRequest{
-				StatusCode: 404,
-				Headers:    stringmap.StringMap{"name": "value"},
-			},
+			&event.HttpRequest{Metadata: stringmap.StringMap{"statusCode": "404", "name": "value"}},
 			true,
 			"status:404",
 		},
