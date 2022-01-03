002aab7b84a273143a73769bffeeee7308135dc4
David Vavra <david.vavra@firma.seznam.cz>
2022-01-03 20:38:14 +0100

    feat: added slo-rules-generator

---
+++ tools/slo-rules-generator/README.md
@@ -0,0 +1,66 @@
+slo-rules-generator is a tool which generates Prometheus recording rules based in input defintion of SLO domain. The generated rules are necessary for SLO based alerting - error budget exhaustion and burn-rate based alerts.
+
+See [./slo-domains.yaml.example](./slo-domains.yaml.example) for commented example of input configuration.
+
+## Usage
+
+ 1. Run `go version` to verify that you have `Go` installed - if not, refer to [golang website](https://golang.org/doc/install)
+ 1. Build `slo-rules-generator` by running `go build .`
+ 1. Run `slo-rules-generator` with `slo-domain.yaml.example` as an argument by running the following command:
+    ```bash
+    slo-rules-generator slo-domains.yaml.example
+    ```
+## Metrics in generated output
+### slo:stable_version
+- used in order to link given domain to specific team (label `team`)
+- `enabled="true|false"` disable burn_rate, error budget alerts
+- `escalate` label documents first escalation level for the given domain
+
+Example:
+```
+slo:stable_version{enabled="true", escalate="sre-team@company.org", namespace="test", slo_domain="example-domain", slo_version="1", team="example-team@company.org"}
+```
+### slo:violation_ratio_threshold
+- holds value of threshold for given `slo_version, slo_domain, slo_class, slo_type, namespace`
+- additional labels simplify values visualization in Grafana for latency-related SLO types - `percentile`, `le` (same as for `le` in Prometheus histograms, documents latency threshold)
+
+Example:
+```
+slo:violation_ratio_threshold{le="0.6", namespace="test", percentile="90", slo_class="critical", slo_domain="example-domain", slo_type="latency90", slo_version="1"}
+	0.9
+slo:violation_ratio_threshold{le="12.0", namespace="test", percentile="99", slo_class="critical", slo_domain="example-domain", slo_type="latency99", slo_version="1"}
+	0.99
+slo:violation_ratio_threshold{namespace="test", slo_class="critical", slo_domain="example-domain", slo_type="availability", slo_version="1"}
+	0.9
+```
+### slo:burn_rate_threshold
+- modifier for slo:burn_rate based alerts' threshold
+- default values are usually reasonable, make sure you read chapters on SLO from [SRE workbook](https://sre.google/workbook/table-of-contents/) before even considering to change these
+
+Example:
+```
+slo:burn_rate_threshold{namespace="test", slo_class="critical", slo_domain="example-domain", slo_time_range="1d", slo_type="availability", slo_version="1"}
+	2.8
+slo:burn_rate_threshold{namespace="test", slo_class="critical", slo_domain="example-domain", slo_time_range="1d", slo_type="latency90", slo_version="1"}
+	2.8
+slo:burn_rate_threshold{namespace="test", slo_class="critical", slo_domain="example-domain", slo_time_range="1d", slo_type="latency99", slo_version="1"}
+	2.8
+slo:burn_rate_threshold{namespace="test", slo_class="critical", slo_domain="example-domain", slo_time_range="1h", slo_type="availability", slo_version="1"}
+	13.44
+slo:burn_rate_threshold{namespace="test", slo_class="critical", slo_domain="example-domain", slo_time_range="1h", slo_type="latency90", slo_version="1"}
+	13.44
+slo:burn_rate_threshold{namespace="test", slo_class="critical", slo_domain="example-domain", slo_time_range="1h", slo_type="latency99", slo_version="1"}
+	13.44
+slo:burn_rate_threshold{namespace="test", slo_class="critical", slo_domain="example-domain", slo_time_range="3d", slo_type="availability", slo_version="1"}
+	1
+slo:burn_rate_threshold{namespace="test", slo_class="critical", slo_domain="example-domain", slo_time_range="3d", slo_type="latency90", slo_version="1"}
+	1
+slo:burn_rate_threshold{namespace="test", slo_class="critical", slo_domain="example-domain", slo_time_range="3d", slo_type="latency99", slo_version="1"}
+	1
+slo:burn_rate_threshold{namespace="test", slo_class="critical", slo_domain="example-domain", slo_time_range="6h", slo_type="availability", slo_version="1"}
+	5.6
+slo:burn_rate_threshold{namespace="test", slo_class="critical", slo_domain="example-domain", slo_time_range="6h", slo_type="latency90", slo_version="1"}
+	5.6
+slo:burn_rate_threshold{namespace="test", slo_class="critical", slo_domain="example-domain", slo_time_range="6h", slo_type="latency99", slo_version="1"}
+	5.6
+```
---
+++ tools/slo-rules-generator/slo-rules-generator_test.go
@@ -0,0 +1,241 @@
+package main
+
+import (
+	"testing"
+	"time"
+
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/pkg/rulefmt"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
+)
+
+func MustMarshall(input interface{}) string {
+	out, err := yaml.Marshal(input)
+	if err != nil {
+		panic(err)
+	}
+	return string(out)
+}
+
+func TestDomainASRuleGroup(t *testing.T) {
+	testTable := []struct {
+		name, inputConf string
+		expectedOutput  []rulefmt.RuleGroup
+	}{
+		{
+			"Domain without classes",
+			`domain-without-classes:
+  enabled: false
+  namespace: production
+  version: 1
+  alerting:
+    team: team.a@company.org
+    escalate: team.sre@company.org`,
+			[]rulefmt.RuleGroup{
+				{
+					Name:     "slo_v1_slo_exporter_domain-without-classes",
+					Interval: model.Duration(4 * time.Minute),
+					Rules: []rulefmt.RuleNode{
+						{
+							Record: yamlStr("slo:stable_version"),
+							Expr:   yamlStr("1"),
+							Labels: Labels{
+								"slo_domain": "domain-without-classes",
+								"namespace":  "production",
+								"team":       "team.a@company.org",
+								"escalate":   "team.sre@company.org",
+								"enabled":    "false",
+								"version":    "1",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			"Domain with single class and type - no burn rate alerting override",
+			`test-domain:
+  enabled: false
+  namespace: production
+  version: 1
+  alerting:
+    team: team.a@company.org
+    escalate: team.sre@company.org
+  classes:
+    critical:
+      availability: {slo_threshold: 0.99}`,
+			[]rulefmt.RuleGroup{
+				{
+					Name:     "slo_v1_slo_exporter_test-domain",
+					Interval: model.Duration(4 * time.Minute),
+					Rules: []rulefmt.RuleNode{
+						{
+							Record: yamlStr("slo:stable_version"),
+							Expr:   yamlStr("1"),
+							Labels: Labels{
+								"slo_domain": "test-domain",
+								"namespace":  "production",
+								"team":       "team.a@company.org",
+								"escalate":   "team.sre@company.org",
+								"enabled":    "false",
+								"version":    "1",
+							},
+						},
+					},
+				},
+				{
+					Name:     "slo_v1_slo_exporter_test-domain_critical",
+					Interval: model.Duration(4 * time.Minute),
+					Rules: []rulefmt.RuleNode{
+						{
+							Record: yamlStr("slo:burn_rate_threshold"),
+							Expr:   yamlStr("13.44"),
+							Labels: Labels{
+								"slo_domain":     "test-domain",
+								"slo_class":      "critical",
+								"namespace":      "production",
+								"version":        "1",
+								"slo_type":       "availability",
+								"slo_time_range": "1h",
+							},
+						},
+						{
+							Record: yamlStr("slo:burn_rate_threshold"),
+							Expr:   yamlStr("5.6"),
+							Labels: Labels{
+								"slo_domain":     "test-domain",
+								"slo_class":      "critical",
+								"namespace":      "production",
+								"version":        "1",
+								"slo_type":       "availability",
+								"slo_time_range": "6h",
+							},
+						},
+						{
+							Record: yamlStr("slo:burn_rate_threshold"),
+							Expr:   yamlStr("2.8"),
+							Labels: Labels{
+								"slo_domain":     "test-domain",
+								"slo_class":      "critical",
+								"namespace":      "production",
+								"version":        "1",
+								"slo_type":       "availability",
+								"slo_time_range": "1d",
+							},
+						},
+						{
+							Record: yamlStr("slo:burn_rate_threshold"),
+							Expr:   yamlStr("1"),
+							Labels: Labels{
+								"slo_domain":     "test-domain",
+								"slo_class":      "critical",
+								"namespace":      "production",
+								"version":        "1",
+								"slo_type":       "availability",
+								"slo_time_range": "3d",
+							},
+						},
+						{
+							Record: yamlStr("slo:violation_ratio_threshold"),
+							Expr:   yamlStr("0.99"),
+							Labels: Labels{
+								"slo_domain": "test-domain",
+								"slo_class":  "critical",
+								"namespace":  "production",
+								"version":    "1",
+								"slo_type":   "availability",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			"Domain with single class and type - burn rate alerting override",
+			`test-domain:
+  enabled: false
+  namespace: production
+  version: 1
+  alerting:
+    team: team.a@company.org
+    escalate: team.sre@company.org
+    burn_rate_thresholds:
+      - condition: {class: 'critical',      slo_type: 'availability',    time_range: '3d'}
+        value: 100
+  classes:
+    critical:
+      availability: {slo_threshold: 0.99}`,
+			[]rulefmt.RuleGroup{
+				{
+					Name:     "slo_v1_slo_exporter_test-domain",
+					Interval: model.Duration(4 * time.Minute),
+					Rules: []rulefmt.RuleNode{
+						{
+							Record: yamlStr("slo:stable_version"),
+							Expr:   yamlStr("1"),
+							Labels: Labels{
+								"slo_domain": "test-domain",
+								"namespace":  "production",
+								"team":       "team.a@company.org",
+								"escalate":   "team.sre@company.org",
+								"enabled":    "false",
+								"version":    "1",
+							},
+						},
+					},
+				},
+				{
+					Name:     "slo_v1_slo_exporter_test-domain_critical",
+					Interval: model.Duration(4 * time.Minute),
+					Rules: []rulefmt.RuleNode{
+						{
+							Record: yamlStr("slo:burn_rate_threshold"),
+							Expr:   yamlStr("100"),
+							Labels: Labels{
+								"slo_domain":     "test-domain",
+								"slo_class":      "critical",
+								"namespace":      "production",
+								"version":        "1",
+								"slo_type":       "availability",
+								"slo_time_range": "3d",
+							},
+						},
+						{
+							Record: yamlStr("slo:violation_ratio_threshold"),
+							Expr:   yamlStr("0.99"),
+							Labels: Labels{
+								"slo_domain": "test-domain",
+								"slo_class":  "critical",
+								"namespace":  "production",
+								"version":    "1",
+								"slo_type":   "availability",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			data := SloConfiguration{}
+			err := yaml.Unmarshal([]byte(testCase.inputConf), &data)
+			if err != nil {
+				t.Errorf("Unable to unmarshal input data: %v", err)
+			}
+			outputRuleGroups := []rulefmt.RuleGroup{}
+			for domainName, domainConfig := range data {
+				if errs := domainConfig.IsValid(); len(errs) > 0 {
+					t.Error(errs)
+				}
+				outputRuleGroups = append(outputRuleGroups, domainConfig.AsRuleGroups(domainName)...)
+			}
+			if err != nil {
+				t.Error(err)
+			}
+			assert.Equal(t, MustMarshall(testCase.expectedOutput), MustMarshall(outputRuleGroups))
+		})
+	}
+}
