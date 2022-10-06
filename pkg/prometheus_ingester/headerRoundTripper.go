455774e2bd623ef1ee4474a614cceba2f5f12dd1
Vlk, Ondrej <ondrej.vlk@firma.seznam.cz>
2022-10-06 12:39:27 +0200

    refactor: do not copy headers in headersRoundtripper

--- pkg/prometheus_ingester/headerRoundTripper.go
+++ pkg/prometheus_ingester/headerRoundTripper.go
@@ -9,22 +9,11 @@ type httpHeadersRoundTripper struct {
 
 // RoundTrip implements the http.RoundTripper interface.
 func (h httpHeadersRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
-	// The specification of http.RoundTripper says that it shouldn't mutate
-	// the request so make a copy of req.Header since this is all that is
-	// modified. https://pkg.go.dev/net/http#RoundTripper
-	r2 := new(http.Request)
-	*r2 = *r
-	r2.Header = make(http.Header, len(r.Header)+len(h.headers))
-	// copy existing headers
-	for k, v := range r.Header {
-		r2.Header[k] = v
-	}
-
-	// add new headers
+	// We use RoundTripper to inject HTTP headers even though it is not advised,
+	// but the Prometheus client does not allow us to do it otherwise.
 	for k, v := range h.headers {
-		r2.Header.Set(k, v)
+		r.Header.Set(k, v)
 	}
 
-	r = r2
 	return h.roudTripper.RoundTrip(r)
 }
